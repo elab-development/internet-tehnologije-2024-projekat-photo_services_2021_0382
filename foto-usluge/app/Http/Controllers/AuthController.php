4bca281a1315d81f0bd4aeec09116ebc0f50bcdd
Ivanaaa <ivanapantelic02.ip@gmail.com>
2024-12-07 18:03:24 +0100

    Auth controller za registrovanje i login, kreiranje ruta

---
+++ foto-usluge/app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
+
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+       $token = $user->createToken('auth_token')->plainTextToken;
+
+       return response()->json([
+           'message' => 'User registered!',
+           'user' => $user,
+           'token' => $token,
+       ], 201); 
+        
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($validated)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+         $token = $user->createToken('auth_token')->plainTextToken;
+         
+        return response()->json([
+            'message' => 'User logged in.',
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+   
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
+
