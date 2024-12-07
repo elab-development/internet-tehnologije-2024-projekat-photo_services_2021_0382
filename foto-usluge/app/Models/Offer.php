424773a3ed7b9393fa040a987b3eea22ccf1cda0
Ivanaaa <ivanapantelic02.ip@gmail.com>
2024-12-07 17:56:37 +0100

    Modeli i veze izmedju njih

---
+++ foto-usluge/app/Models/Offer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Offer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'status', 
+        'price',
+        'payment_type', 
+        'date',
+        'notes', 
+        'service_id', 
+        'buyer_id', 
+        'seller_id', 
+    ];
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    // Relacija sa kupcem
+    public function buyer()
+    {
+        return $this->belongsTo(User::class, 'buyer_id');
+    }
+
+    // Relacija sa prodavcem
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+}
---
+++ foto-usluge/app/Models/Service.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'price', 
+        'description', 
+        'service_category_id', 
+    ];
+
+    
+    public function category()
+    {
+        return $this->belongsTo(ServiceCategory::class, 'service_category_id');
+    }
+
+    public function offers()
+    {
+        return $this->hasMany(Offer::class);
+    }
+}
